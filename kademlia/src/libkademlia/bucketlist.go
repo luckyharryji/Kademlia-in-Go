a5d1ff23141e43180093db338f57fe8a96eb3913
luckyharryji <xiangyu.ji@outlook.com>
2016-04-20 18:43:38 -0500

    rewrite bucket list object

---
+++ kademlia/src/libkademlia/bucketlist.go
@@ -0,0 +1,59 @@
+package libkademlia
+import (
+    "fmt"
+)
+
+type BucketNode struct {
+    contact Contact
+    next, prev  *BucketNode
+}
+
+type BucketList struct {
+    head, tail *BucketNode
+    length int
+}
+
+func (list *BucketList) First() *BucketNode {
+    return list.head
+}
+
+func (node *BucketNode) Next() *BucketNode {
+    return node.next
+}
+
+func (node *BucketNode) Prev() *BucketNode {
+    return node.prev
+}
+
+func (list *BucketList) Push(contact Contact) *BucketList {
+    n := &BucketNode{contact: contact}
+    if list.head == nil {
+        list.head = n
+    } else {
+        list.tail.next = n
+        n.prev = list.tail
+    }
+    list.tail = n
+    list.length += 1
+    return list
+}
+
+func (list *BucketList) Find(contact Contact) *BucketNode {
+    found := false
+    var ret *BucketNode = nil
+    for n := list.First(); n != nil && !found; n = n.Next() {
+        if n.contact.NodeID.Equals(contact.NodeID) {
+            found = true
+            ret = n
+        }
+    }
+    return ret
+}
+
+func (list *BucketList) DeleteFrontInsert(contact Contact) *BucketList {
+    frontNode := list.head
+    list.head = frontNode.next
+    list.head.prev = nil
+    list.Push(contact)
+    return list
+}
---
+++ kademlia/src/libkademlia/routing.go
@@ -0,0 +1,103 @@
+package libkademlia
+
+import (
+	"container/list"
+	"sort"
+)
+
+/*
+By defination in libkademlia
+const (
+	alpha = 3
+	b     = 8 * IDBytes
+	k     = 20
+)
+*/
+
+type RoutingTable struct {
+	Buckets [b] *BucketList
+}
+
+type ContactForSort struct {
+	contact Contact
+	id      ID
+}
+
+type Contacts []ContactForSort
+
+func NewRoutingTable() (table *RoutingTable) {
+	table = new(RoutingTable)
+	for i := 0; i < 8 * IDBytes; i++ {
+		table.BucketLists[i] = list.New()
+	}
+	return
+}
+
+func (table *RoutingTable) UpDate(selfID ID, contact Contact) {
+	id_table := table.SelfContact.NodeID
+	id := contact.NodeID
+	prefixlen := id.Xor(id_table).PrefixLen()
+	bucket := table.BucketLists[prefixlen]
+	var element *list.Element
+	for e := bucket.Front(); e != nil; e = e.Next() {
+		if e.Value.(*Contact).NodeID.Equals(contact.NodeID) {
+			element = e
+			break
+		}
+	}
+	if element == nil {
+		if bucket.Len() <= k {
+			bucket.PushFront(contact)
+		}
+		//error := Ping
+		//if error != nil {
+		//	bucket.Remove(contact)
+		//}
+	} else {
+		bucket.MoveToFront(element)
+	}
+}
+
+func CreateShortList(shortlist Contacts, list list.List, count int, id ID) {
+	for e := list.Front(); e != nil; e = e.Next() {
+		temp := ContactForSort{e.Value.(Contact), id}
+		shortlist = append(shortlist, temp)
+		count++
+	}
+}
+
+func (table *RoutingTable) FindCloset(id ID) []Contact {
+	prefixlen := id.Xor(table.SelfContact.NodeID).PrefixLen()
+	bucket := table.BucketLists[prefixlen]
+	var shortlist Contacts
+	count := 0
+	CreateShortList(shortlist, *bucket, count, id)
+	for i := 1; (prefixlen-i >= 0 || prefixlen+i < IDBytes*8) && count <= k; i++ {
+		if prefixlen-i >= 0 {
+			bucket = table.BucketLists[prefixlen-i]
+			CreateShortList(shortlist, *bucket, count, id)
+		}
+		if prefixlen+i < IDBytes*8 {
+			bucket = table.BucketLists[prefixlen+i]
+			CreateShortList(shortlist, *bucket, count, id)
+		}
+	}
+	sort.Sort(shortlist)
+	result := make([]Contact, k)
+	for i := 0; i < k; i++ {
+		result = append(result, shortlist[i].contact)
+	}
+	return result
+}
+
+func (list Contacts) Len() int {
+	return len(list)
+}
+
+func (list Contacts) Swap(i, j int) {
+	list[i], list[j] = list[j], list[i]
+}
+
+func (list Contacts) Less(i, j int) bool {
+	return list[i].contact.NodeID.Xor(list[i].id).Less(list[j].contact.NodeID.Xor(list[j].id))
+}
